54f7e7f8f8fc3059b07f9ec00c1596076a987c2b
Jefferson <jefferson.silvaquinto@gmail.com>
2023-12-19 15:37:07 -0500

    adding new route image

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './Hero.css'
 import { Link } from 'react-router-dom';
-import SliderProducts from '../SliderProducts/SliderProducts';
 
 const Hero = () => {
     return (
         <>            
             <section className='hero'>
                 <div className="hero-content-img">
-                    <img className='hero-image-model' src="./src/assets/img/models/model-main.jpg" alt="Foto de ahmet öktem: https://www.pexels.com/es-es/foto/gafas-de-sol-18978812/" />
+                    <img className='hero-image-model' src="../public/assets/img/models/model-main.jpg" alt="Foto de ahmet öktem: https://www.pexels.com/es-es/foto/gafas-de-sol-18978812/" />
                 </div>
                 <div className="hero-context">
                     <h2 className="hero-h2">Beautiful summer designs for you</h2>
