02f26c97bf0a497def7c05f6ce064d36ffa0d9d9
Jefferson <jefferson.silvaquinto@gmail.com>
2023-12-19 15:41:28 -0500

    trying it

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <>            
             <section className='hero'>
                 <div className="hero-content-img">
-                    <img className='hero-image-model' src="../public/assets/img/models/model-main.jpg" alt="Foto de ahmet öktem: https://www.pexels.com/es-es/foto/gafas-de-sol-18978812/" />
+                    <img className='hero-image-model' src="../assets/img/models/model-main.jpg" alt="Foto de ahmet öktem: https://www.pexels.com/es-es/foto/gafas-de-sol-18978812/" />
                 </div>
                 <div className="hero-context">
                     <h2 className="hero-h2">Beautiful summer designs for you</h2>
